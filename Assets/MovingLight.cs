31012d87a3998dadac32c1180ec55d543ec6ed69
Chaeho Shin <gshin3@live.com>
2024-12-02 09:57:03 -0800

    change lighting moving clamp to -90, 90

--- Assets/MovingLight.cs
+++ Assets/MovingLight.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MovingLight : MonoBehaviour
 {
+    float degTracker = 0.0f;
+    float factor = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,16 @@ public class MovingLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.0f,20.0f*Time.deltaTime, 0.0f, Space.World);
+        degTracker += factor * 20.0f * Time.deltaTime;
+        
+        if (degTracker > 90.0f) {
+            transform.rotation = Quaternion.Euler(60.0f, 90.0f, 0.0f);
+            factor = -1.0f;
+        }
+        else if(degTracker < -90.0f) {
+            transform.rotation = Quaternion.Euler(60.0f, -90.0f, 0.0f);
+            factor = 1.0f;
+        }
+        transform.Rotate(0.0f, factor * 20.0f * Time.deltaTime, 0.0f, Space.World);
     }
 }
